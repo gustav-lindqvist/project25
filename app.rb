758d690f9473ee18d5603795654cb489bb62ad4b
gustav-lindqvist <gustav.lindqvist@elev.ga.ntig.se>
2025-02-18 09:18:09 +0100

    fyllt startsidan med lite mer innehåll som text och bilder

--- app.rb
+++ app.rb
@@ -18,4 +18,15 @@ get '/game' do
     slim :game
   end
 
+  # Hantera snurr-knappen
+post '/spin' do
+    symbols = ['🔥', '⭐', '🍋', '🍒', '🎰', '7']
+    result = [symbols.sample, symbols.sample, symbols.sample].join(' ')
+    bet = params[:bet].to_f
+    win = rand(0..bet * 5).round(2) # Slumpmässig vinst
   
+    # Spara i databasen
+    db.execute("INSERT INTO spins (user_id, result, bet_amount, win_amount) VALUES (?, ?, ?, ?)", [1, result, bet, win])
+  
+    "Result: #{result} | Win: #{win}"
+end
