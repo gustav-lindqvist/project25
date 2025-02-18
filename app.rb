69e12ad4f69aab5d48da3277ef54ae28ceed23a4
gustav-lindqvist <gustav.lindqvist@elev.ga.ntig.se>
2025-02-18 09:12:19 +0100

    anlöt till db filen och har skapat slimfilerna och kopplat även dom till ruby

--- app.rb
+++ app.rb
@@ -3,3 +3,19 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'becrypt'
+
+# Anslut till databasen
+db = SQLite3::Database.new('casino.db')
+db.results_as_hash = true
+
+# Startsida
+get '/' do
+    slim :index
+  end
+
+  # Spelsida
+get '/game' do
+    slim :game
+  end
+
+  
